cc76f84cf1cfa7c115b2b4b4885409b92e10a018
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-06-02 14:51:34 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+function App() {
+  return (
+    <div>
+      <h3>Hello World!</h3>
+      <Button> My Button</Button>
+    </div>
+  );
+}
+
+export default App;
