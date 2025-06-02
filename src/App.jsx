33b20c42ec0bba2256f4589b2067b4c1997e8564
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-06-02 15:26:13 +0530

    Material UI Basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import { Button } from "@mui/material";
+import { Button, Box } from "@mui/material";
+
+const MySpecialBox = (props) => {
+  return (
+    <Box>
+      label:{props.label} {props.children}
+    </Box>
+  );
+};
 function App() {
   return (
     <div>
       <h3>Hello World!</h3>
-      <Button> My Button</Button>
+      <Button
+        // component="a"
+        sx={{ bgcolor: "green" }}
+        variant="contained"
+        disableElevation
+        onClick={() => console.log("Clicked Me!")}
+      >
+        My Button
+      </Button>
+
+      <MySpecialBox label="MY BOX">
+        <Button variant="contained">My Special Box</Button>
+      </MySpecialBox>
     </div>
   );
 }
